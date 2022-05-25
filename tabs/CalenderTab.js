6d422bf4bd46a48ad49e898e5403b3a600de1cff
SeungJin Kim <64381@hufs.ac.kr>
2022-05-25 20:32:14 +0900

    calender 라이브러리 설치 + 캘린더 추가

--- tabs/CalenderTab.js
+++ tabs/CalenderTab.js
@@ -1,9 +1,62 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import { Calendar, LocaleConfig } from 'react-native-calendars';
 
 export default function CalenderTab() {
   return (
     <View>
       <Text>CalenderTab</Text>
+      <View style={styles.calender}>
+        <Calendar />
+      </View>
     </View>
   );
 }
+
+LocaleConfig.locales['kr'] = {
+  monthNames: [
+    '1월',
+    '2월',
+    '3월',
+    '4월',
+    '5월',
+    '6월',
+    '7월',
+    '8월',
+    '9월',
+    '10월',
+    '11월',
+    '12월',
+  ],
+  monthNamesShort: [
+    '1월',
+    '2월',
+    '3월',
+    '4월',
+    '5월',
+    '6월',
+    '7월',
+    '8월',
+    '9월',
+    '10월',
+    '11월',
+    '12월',
+  ],
+  dayNames: [
+    '일요일',
+    '월요일',
+    '화요일',
+    '수요일',
+    '목요일',
+    '금요일',
+    '토요일',
+  ],
+  dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+  today: '오늘',
+};
+LocaleConfig.defaultLocale = 'kr';
+
+const styles = StyleSheet.create({
+  calender: {
+    alignContent: 'center',
+  },
+});
