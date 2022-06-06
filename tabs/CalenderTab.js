036decc00194a6c79f2e09c5975d1de2b981a4a8
chjunyo <101381244+chjunyo@users.noreply.github.com>
2022-06-07 01:01:03 +0900

    License Component etc.
    package.json 정리 및 License 컴포넌트 생성

--- tabs/CalenderTab.js
+++ tabs/CalenderTab.js
@@ -1,66 +1,89 @@
 import { Text, View, StyleSheet } from 'react-native';
 import { Calendar, LocaleConfig } from 'react-native-calendars';
-
-
-
+import { useState } from 'react';
+import License from '../components/License';
+import CircleButton from '../components/CircleButton';
 
 //날짜 사이의 기간을 색칠해서 리턴하는 함수
 export function getPeriod(dataArr) {
-  const period = {}
+  const period = {};
 
   for (let i = 0; i < dataArr.length; i++) {
-
     const date = new Date(dataArr[i].startDate);
     const dates = [];
-    
+
     //dates 안에 두 날짜와 그 사이의 날짜를 추가
     while (date <= dataArr[i].endDate) {
       dates.push(new Date(date));
       date.setDate(date.getDate() + 1);
     }
-    if(!(dataArr[i].endDate in dates)) dates.push(dataArr[i].endDate)
-
+    if (!(dataArr[i].endDate in dates)) dates.push(dataArr[i].endDate);
 
     //dates 안의 날짜를 key로 갖는 오브젝트 생성
     for (let k = 0; k < dates.length; k++) {
       const key = dates[k].toISOString().substring(0, 10);
-      if (!(key in period)) period[key] = {periods: []}
-      else period[key] = {periods: [...period[key].periods]}
+      if (!(key in period)) period[key] = { periods: [] };
+      else period[key] = { periods: [...period[key].periods] };
     }
-
-    
 
     //추가된 할 일의 name을 비교하여 이미 추가된 날짜인지 판별 후 아니면 새로 추가
     for (let k = 0; k < dates.length; k++) {
       const key = dates[k].toISOString().substring(0, 10);
-      const prevKey = k !== 0 ? dates[k-1].toISOString().substring(0, 10) : ''
-      const color = dataArr[i].tags[0]?.color === undefined ? "#656A8F" : dataArr[i].tags[0].color
-      let isUnique = true
-      
-      for(let j=0;j<period[key].periods.length;j++){
-        if (period[key].periods[j].name === dataArr[i].name) isUnique = false
+      const prevKey =
+        k !== 0 ? dates[k - 1].toISOString().substring(0, 10) : '';
+      const color =
+        dataArr[i].tags[0]?.color === undefined
+          ? '#656A8F'
+          : dataArr[i].tags[0].color;
+      let isUnique = true;
+
+      for (let j = 0; j < period[key].periods.length; j++) {
+        if (period[key].periods[j].name === dataArr[i].name) isUnique = false;
       }
 
-      if (k===0 && isUnique) period[key].periods.push({startingDay: true, color: color, name: dataArr[i].name});
-      else if(k=== dates.length-1 && isUnique) period[key].periods.push({endingDay: true, color: period[prevKey].periods[0].color , name: dataArr[i].name});
-      else if(isUnique) period[key].periods.push({color: period[prevKey].periods[0].color, name: dataArr[i].name});
+      if (k === 0 && isUnique)
+        period[key].periods.push({
+          startingDay: true,
+          color: color,
+          name: dataArr[i].name,
+        });
+      else if (k === dates.length - 1 && isUnique)
+        period[key].periods.push({
+          endingDay: true,
+          color: period[prevKey].periods[0].color,
+          name: dataArr[i].name,
+        });
+      else if (isUnique)
+        period[key].periods.push({
+          color: period[prevKey].periods[0].color,
+          name: dataArr[i].name,
+        });
     }
-    
   }
 
   return period;
 }
 
 export default function CalenderTab(todos) {
-
-  const markedDates = getPeriod(todos.todos.data)
+  const markedDates = getPeriod(todos.todos.data);
+  const [showLicense, setShowLicense] = useState(false);
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>CalenderTab</Text>
       <View style={styles.calender}>
         <Calendar markingType="multi-period" markedDates={markedDates} />
       </View>
+      <CircleButton
+        onPress={() => setShowLicense(true)}
+        style={styles.licenseButton}
+        width={100}
+        height={30}
+        backgroundColor="#ededed"
+      >
+        <Text>LICENSE</Text>
+      </CircleButton>
+      {showLicense ? <License onClose={() => setShowLicense(false)} /> : <></>}
     </View>
   );
 }
@@ -111,7 +134,17 @@ LocaleConfig.defaultLocale = 'kr';
 
 //스타일
 const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+  },
+
   calender: {
     alignContent: 'center',
+    flex: 1,
+  },
+
+  licenseButton: {
+    margin: 20,
+    alignSelf: 'flex-end',
   },
 });
